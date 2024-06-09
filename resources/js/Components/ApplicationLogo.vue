fa1fb50a092c9625cfdbf4c994049d62abce3ec0
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-06-09 23:59:39 +0800

    update aplication logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-      <!-- Your logo or application header here -->
       <img :src="faviconSrc" :alt="altText" :class="imgClass">
     </div>
   </template>
@@ -8,15 +7,12 @@
   <script setup>
   import { ref } from 'vue';
   
-  // Properti untuk ukuran gambar
   const imgWidth = ref('');
   const imgHeight = ref('');
   
-  // Properti untuk favicon
   const faviconSrc = '/favicon.ico';
   const altText = 'Favicon';
   
-  // Gabungkan kelas untuk gambar
   const imgClass = `${imgWidth.value} ${imgHeight.value}`;
   </script>
   
