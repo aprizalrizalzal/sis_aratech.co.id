51259b962af4c62f6a86ec3131a724fac70e3677
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-06-09 22:06:03 +0800

    update time out and language

--- config/auth.php
+++ config/auth.php
@@ -94,8 +94,8 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
+            'expire' => 240,
+            'throttle' => 240,
         ],
     ],
 
@@ -110,6 +110,6 @@ return [
     |
     */
 
-    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
+    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 43200),
 
 ];
