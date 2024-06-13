9cc8eb9ecc1842afbfe3d4d1881ef05ceeb47c4e
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-06-14 01:43:52 +0800

    update animasi

--- resources/js/Pages/Unauthorized/Unauthorized.vue
+++ resources/js/Pages/Unauthorized/Unauthorized.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Head, Link } from '@inertiajs/vue3';
 </script>
 
@@ -7,18 +8,15 @@ import { Head, Link } from '@inertiajs/vue3';
     <Head title="Unauthorized" />
     <div class="flex flex-col justify-center items-center h-screen bg-gray-100">
         <div class="text-center">
-            <h1 class="mb-4 text-6xl font-semibold text-red-500">404</h1>
+            <h1 class="mb-4 text-6xl font-semibold text-green-800 animate-ping">404</h1>
             <p class="mb-4 text-lg text-gray-600">Oops! Looks like you're lost.</p>
-            <div class="animate-bounce">
-                <svg class="mx-auto h-16 w-16 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
-                </svg>
+            <div class="animate-pulse">
+                <ApplicationLogo class="inline-block h-64 w-64 fill-current p-4" />
             </div>
-            <p class="mt-4 text-gray-600">Let's get you back </p>
+            <p class="text-gray-600">Let's get you back </p>
             <Link :href="route('show.welcome')"
-                class="mt-8 inline-block bg-red-700 text-white py-2 px-4 rounded hover:bg-red-500 transition duration-300">
-            Kembali ke Beranda
+                class="mt-8 inline-block bg-green-800 text-white py-2 px-4 rounded-md hover:bg-green-600 transition duration-150">
+            Return to Home
             </Link>.
         </div>
     </div>
