a33a0a2f1316b1f64ca217eb3a82865e59391d83
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-06-22 11:07:14 +0700

    update table seeder

--- database/seeders/CarouselsTableSeeder.php
+++ database/seeders/CarouselsTableSeeder.php
@@ -14,19 +14,19 @@ class CarouselsTableSeeder extends Seeder
     public function run(): void
     {
         Carousel::create([
-            'image_path' => 'storage/images/carousels/1',
+            'image_path' => 'storage/images/carousels/1.png',
             'alt' => 'Satu',
         ]);
         Carousel::create([
-            'image_path' => 'storage/images/Carousels/2',
+            'image_path' => 'storage/images/Carousels/2.png',
             'alt' => 'Dua',
         ]);
         Carousel::create([
-            'image_path' => 'storage/images/Carousels/3',
+            'image_path' => 'storage/images/Carousels/3.png',
             'alt' => 'Tiga',
         ]);
         Carousel::create([
-            'image_path' => 'storage/images/Carousels/4',
+            'image_path' => 'storage/images/Carousels/4.png',
             'alt' => 'Empat',
         ]);
     }
