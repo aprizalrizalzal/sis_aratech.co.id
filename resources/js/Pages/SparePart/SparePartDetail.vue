4dd0ff4acc2950d73a818f92d092b993bafa70ad
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-07-18 09:43:31 +0700

    end image detail

--- resources/js/Pages/SparePart/SparePartDetail.vue
+++ resources/js/Pages/SparePart/SparePartDetail.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+
 const props = defineProps({
     canLogin: Boolean,
     canRegister: Boolean,
-
     sparePart: Object,
 });
 
 const imageSpareParts = ref(props.sparePart.image_spare_parts || []);
+const initialImage = ref(props.sparePart.image_path);
+const selectedImage = ref(initialImage.value);
 
 const formatCurrency = (value) => {
     return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(value);
@@ -16,17 +18,31 @@ const formatCurrency = (value) => {
 const formatDescription = (description) => {
     return description.replace(/\n/g, '<br>');
 };
+
+const selectImage = (imagePath) => {
+    selectedImage.value = (selectedImage.value === imagePath) ? initialImage.value : imagePath;
+};
+
+watch(() => props.sparePart.image_path, (newValue) => {
+    initialImage.value = newValue;
+    selectedImage.value = newValue;
+});
 </script>
 
 <template>
     <h1 class="text-green-900 font-bold my-4 text-center sm:text-lg text-md">{{ sparePart.name }}</h1>
     <div class="grid sm:grid-cols-2 grid-cols-1 gap-4">
         <div class="h-full">
-            <img :src="sparePart.image_path" :alt="sparePart.name" class="rounded-md shadow-md">
-            <div v-if="imageSpareParts.length"class="mt-6">
+            <img :src="selectedImage" :alt="sparePart.name" class="rounded-md shadow-md">
+            <div v-if="imageSpareParts.length" class="mt-6">
                 <div class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
                     <div v-for="(imageSparePart, index) in imageSpareParts" :key="index" class="relative">
-                        <img :src="imageSparePart.image_path" alt="Image Spare Part" class="w-full h-auto rounded-md" />
+                        <img 
+                            :src="imageSparePart.image_path" 
+                            alt="Image Spare Part" 
+                            class="w-full h-auto rounded-md cursor-pointer" 
+                            @click="selectImage(imageSparePart.image_path)" 
+                        />
                     </div>
                 </div>
             </div>
@@ -48,3 +64,14 @@ const formatDescription = (description) => {
         </div>
     </div>
 </template>
+
+<style scoped>
+.relative {
+    position: relative;
+}
+
+.cursor-pointer {
+    cursor: pointer;
+}
+</style>
+
