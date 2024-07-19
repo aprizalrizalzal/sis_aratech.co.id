8c971f617508249f8548f711521e44a221d98504
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-07-19 21:23:53 +0800

    update print

--- resources/js/Pages/Service/ServicePrint.vue
+++ resources/js/Pages/Service/ServicePrint.vue
@@ -21,94 +21,98 @@ const currentUrl = computed(() => {
 onMounted(() => {
     setTimeout(() => {
         window.print();
-        window.close();
     }, 500);
 });
 </script>
 
 <template id="print-service">
+    <div class="m-2 px-2">
 
-    <Head title="Service Print" />
-    <div v-for="header in $page.props.headers" :key="header.id" class="flex items-stretch mb-2 gap-2 text-sm/relaxed">
-        <div>
-            <ApplicationLogo class="block h-20 w-20" />
-        </div>
-        <div id="footer" class="mt-auto">
-            <p class="font-bold text-lg">SIService - {{ header.company }}</p>
-            <p>{{ header.description }}</p>
-            <div v-for="footer in contactFooters" :key="footer.id" class="flex flex-row">
-                <p>{{ footer.value }}</p>
+        <Head :title='"Service " + props.service.service_code' />
+        <div v-for="header in $page.props.headers" :key="header.id"
+            class="flex items-stretch mb-2 gap-2 text-sm/relaxed">
+            <div>
+                <ApplicationLogo class="block h-20 w-20" />
+            </div>
+            <div id="footer" class="mt-auto">
+                <p class="font-bold text-lg">SIService - {{ header.company }}</p>
+                <p>{{ header.description }}</p>
+                <div v-for="footer in contactFooters" :key="footer.id" class="flex flex-row">
+                    <p>{{ footer.value }}</p>
+                </div>
             </div>
         </div>
+        <hr>
+        <table class="table-auto w-full my-2">
+            <tbody>
+                <tr class="font-bold bg-green-50 ">
+                    <td class=" text-green-900">Service Code </td>
+                    <td>{{ service.service_code }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Name</td>
+                    <td>{{ service.customer.user.name }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Email / Phone</td>
+                    <td>{{ service.customer.user.email }}<span> / </span>{{ service.customer.phone }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Address </td>
+                    <td>{{ service.customer.address }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Device Type / Model</td>
+                    <td>{{ service.device.device_type.type_name }}<span> / </span>{{ service.device.model }}</td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Serial Number / Warranty Status </td>
+                    <td>{{ service.device.serial_number }}<span> / </span>{{ service.status_warranty_service.status }}
+                    </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Date Received / Estimated Completion</td>
+                    <td>{{ service.date_received }}<span> / </span>{{ service.estimated_completion }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Problem Description </td>
+                    <td>{{ service.problem_description }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Items Brought </td>
+                    <td>{{ service.items_brought }} </td>
+                </tr>
+                <tr class="font-bold bg-green-50">
+                    <td class=" text-green-900">Status </td>
+                    <td>{{ service.status_service.status }} </td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div id="footer" class="flex gap-4 my-2 justify-between text-sm/relaxed text-left">
+            <div class="flex flex-col gap-10">
+                <p>Admin</p>
+                <span>{{ $page.props.auth.user.name }}</span>
+            </div>
+            <div class="flex flex-col gap-10">
+                <p>Customer</p>
+                <span>{{ service.customer.user.name }}</span>
+            </div>
+            <div class="flex flex-col items-left border px-2 mx-1">
+                <p class="font-bold">Notes!</p>
+                <ul v-for="note in $page.props.notes" :key="note.id">
+                    <li>{{ note.description }}</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <p id="footer" class="mt-2 text-sm/relaxed text-left">
+            Cek perkembangan perangkatmu di <a target="_blank" rel="noopener noreferrer"
+                class="text-green-800 font-bold" :href="currentUrl">{{ currentUrl }}</a> Jangan lupa cek inbox atau spam
+            email Anda ya. Informasi login dan
+            password ada di sana!
+        </p>
     </div>
-    <hr>
-    <table class="table-auto w-full my-2">
-        <tbody>
-            <tr class="font-bold bg-green-50 ">
-                <td class=" text-green-900">Service Code </td>
-                <td>{{ service.service_code }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Name</td>
-                <td>{{ service.customer.user.name }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Email / Phone</td>
-                <td>{{ service.customer.user.email }}<span> / </span>{{ service.customer.phone }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Address </td>
-                <td>{{ service.customer.address }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Device Type / Model</td>
-                <td>{{ service.device.device_type.type_name }}<span> / </span>{{ service.device.model }}</td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Serial Number / Warranty Status </td>
-                <td>{{ service.device.serial_number }}<span> / </span>{{ service.status_warranty_service.status }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Date Received / Estimated Completion</td>
-                <td>{{ service.date_received }}<span> / </span>{{ service.estimated_completion }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Problem Description </td>
-                <td>{{ service.problem_description }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Items Brought </td>
-                <td>{{ service.items_brought }} </td>
-            </tr>
-            <tr class="font-bold bg-green-50">
-                <td class=" text-green-900">Status </td>
-                <td>{{ service.status_service.status }} </td>
-            </tr>
-        </tbody>
-    </table>
-    <hr>
-    <div id="footer" class="flex gap-4 my-2 justify-between text-sm/relaxed text-left">
-        <div class="flex flex-col gap-10">
-            <p>Admin</p>
-            <span>{{ $page.props.auth.user.name }}</span>
-        </div>
-        <div class="flex flex-col gap-10">
-            <p>Customer</p>
-            <span>{{ service.customer.user.name }}</span>
-        </div>
-        <div class="flex flex-col items-left border px-2 mx-1">
-            <p class="font-bold">Notes!</p>
-            <ul v-for="note in $page.props.notes" :key="note.id">
-                <li>{{ note.description }}</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <p id="footer" class="mt-2 text-sm/relaxed text-left">
-        Cek perkembangan perangkatmu di <a target="_blank" rel="noopener noreferrer" class="text-green-800 font-bold"
-            :href="currentUrl">{{ currentUrl }}</a> Jangan lupa cek inbox atau spam email Anda ya. Informasi login dan
-        password ada di sana!
-    </p>
 </template>
 
 <style scoped>
--- resources/js/Pages/ServiceDetail/ServiceDetailPrint.vue
+++ resources/js/Pages/ServiceDetail/ServiceDetailPrint.vue
@@ -34,122 +34,125 @@ const formatCurrency = (value) => {
 onMounted(() => {
     setTimeout(() => {
         window.print();
-        window.close();
     }, 500);
 });
 </script>
 <template id="print-service-detail">
+    <div class="m-2 px-2">
 
-    <Head title="Service Detail Print" />
-    <div v-for="header in $page.props.headers" :key="header.id" class="flex items-stretch mb-2 gap-2 text-sm/relaxed">
-        <div>
-            <ApplicationLogo class="block h-20 w-20" />
-        </div>
-        <div id="footer" class="mt-auto">
-            <p class="font-bold text-lg">SIService - {{ header.company }}</p>
-            <p>{{ header.description }}</p>
-            <div id="text-sm" v-for="footer in contactFooters" :key="footer.id">
-                <p>{{ footer.value }}</p>
+        <Head :title='"Service Detail " + props.serviceDetail.service_detail_code' />
+        <div v-for="header in $page.props.headers" :key="header.id"
+            class="flex items-stretch mb-2 gap-2 text-sm/relaxed">
+            <div>
+                <ApplicationLogo class="block h-20 w-20" />
+            </div>
+            <div id="footer" class="mt-auto">
+                <p class="font-bold text-lg">SIService - {{ header.company }}</p>
+                <p>{{ header.description }}</p>
+                <div id="text-sm" v-for="footer in contactFooters" :key="footer.id">
+                    <p>{{ footer.value }}</p>
+                </div>
             </div>
         </div>
-    </div>
-    <hr>
-    <table class="table-auto w-full my-2">
-        <tbody>
-            <tr class="font-bold bg-green-50">
-                <td class=" text-green-900"> Service Detail Code </td>
-                <td> {{ serviceDetail.service_detail_code }} </td>
-            </tr>
-            <tr>
-                <td class="text-green-900">Email Technician </td>
-                <td> {{ serviceDetail.user.email }} </td>
-            </tr>
-            <tr class="font-bold bg-green-50 ">
-                <td class=" text-green-900">Service Code </td>
-                <td>{{ serviceDetail.service.service_code }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Name</td>
-                <td>{{ serviceDetail.service.customer.user.name }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Email / Phone</td>
-                <td>{{ serviceDetail.service.customer.user.email }}<span> / </span>{{
-                    serviceDetail.service.customer.phone }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Address </td>
-                <td>{{ serviceDetail.service.customer.address }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Device Type / Model</td>
-                <td>{{ serviceDetail.service.device.device_type.type_name }}<span> / </span>{{
-                    serviceDetail.service.device.model }}</td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Serial Number / Warranty Status </td>
-                <td>{{ serviceDetail.service.device.serial_number }}<span> / </span>{{
-                    serviceDetail.service.status_warranty_service.status }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Date Received / Estimated Completion </td>
-                <td>{{ serviceDetail.service.date_received }}<span> / </span>{{
-                    serviceDetail.service.estimated_completion }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Problem Description </td>
-                <td>{{ serviceDetail.service.problem_description }} </td>
-            </tr>
-            <tr>
-                <td class=" text-green-900">Items Brought </td>
-                <td>{{ serviceDetail.service.items_brought }} </td>
-            </tr>
-            <tr class="font-bold bg-green-50">
-                <td class=" text-green-900">Status </td>
-                <td>{{ serviceDetail.service.status_service.status }} </td>
-            </tr>
-            <tr>
-                <td class="text-green-900"> Repair Description </td>
-                <td> {{ serviceDetail.repair_description }} </td>
-            </tr>
-            <tr class="font-bold bg-green-50">
-                <td class="text-green-900"> Spare Part</td>
-                <td class="text-green-900"> Price</td>
-            </tr>
-            <tr v-for="(partUsage, index) in partUsages" :key="partUsage.id">
-                <td class="text-green-900"> {{ index + 1 }}. {{ partUsage.spare_part.name }} </td>
-                <td class="text-green-900"> {{ formatCurrency(partUsage.spare_part.price) }} </td>
-            </tr>
-            <tr class="font-bold bg-green-50">
-                <td class="text-green-900"> Cost </td>
-                <td> {{ formatCurrency(serviceDetail.cost) }} </td>
-            </tr>
-            <tr class="font-bold">
-                <td class="text-green-900"> Total </td>
-                <td> {{ formatCurrency(serviceDetailTotal) }} </td>
-            </tr>
-        </tbody>
-    </table>
-    <hr>
-    <div id="footer" class="flex gap-4 my-2 justify-between text-sm/relaxed text-left">
-        <div class="flex flex-col gap-10">
-            <p>Technician</p>
-            <span>{{ $page.props.auth.user.name }}</span>
+        <hr>
+        <table class="table-auto w-full my-2">
+            <tbody>
+                <tr class="font-bold bg-green-50">
+                    <td class=" text-green-900"> Service Detail Code </td>
+                    <td> {{ serviceDetail.service_detail_code }} </td>
+                </tr>
+                <tr>
+                    <td class="text-green-900">Email Technician </td>
+                    <td> {{ serviceDetail.user.email }} </td>
+                </tr>
+                <tr class="font-bold bg-green-50 ">
+                    <td class=" text-green-900">Service Code </td>
+                    <td>{{ serviceDetail.service.service_code }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Name</td>
+                    <td>{{ serviceDetail.service.customer.user.name }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Email / Phone</td>
+                    <td>{{ serviceDetail.service.customer.user.email }}<span> / </span>{{
+                        serviceDetail.service.customer.phone }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Address </td>
+                    <td>{{ serviceDetail.service.customer.address }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Device Type / Model</td>
+                    <td>{{ serviceDetail.service.device.device_type.type_name }}<span> / </span>{{
+                        serviceDetail.service.device.model }}</td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Serial Number / Warranty Status </td>
+                    <td>{{ serviceDetail.service.device.serial_number }}<span> / </span>{{
+                        serviceDetail.service.status_warranty_service.status }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Date Received / Estimated Completion </td>
+                    <td>{{ serviceDetail.service.date_received }}<span> / </span>{{
+                        serviceDetail.service.estimated_completion }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Problem Description </td>
+                    <td>{{ serviceDetail.service.problem_description }} </td>
+                </tr>
+                <tr>
+                    <td class=" text-green-900">Items Brought </td>
+                    <td>{{ serviceDetail.service.items_brought }} </td>
+                </tr>
+                <tr class="font-bold bg-green-50">
+                    <td class=" text-green-900">Status </td>
+                    <td>{{ serviceDetail.service.status_service.status }} </td>
+                </tr>
+                <tr>
+                    <td class="text-green-900"> Repair Description </td>
+                    <td> {{ serviceDetail.repair_description }} </td>
+                </tr>
+                <tr class="font-bold bg-green-50">
+                    <td class="text-green-900"> Spare Part</td>
+                    <td class="text-green-900"> Price</td>
+                </tr>
+                <tr v-for="(partUsage, index) in partUsages" :key="partUsage.id">
+                    <td class="text-green-900"> {{ index + 1 }}. {{ partUsage.spare_part.name }} </td>
+                    <td class="text-green-900"> {{ formatCurrency(partUsage.spare_part.price) }} </td>
+                </tr>
+                <tr class="font-bold bg-green-50">
+                    <td class="text-green-900"> Cost </td>
+                    <td> {{ formatCurrency(serviceDetail.cost) }} </td>
+                </tr>
+                <tr class="font-bold">
+                    <td class="text-green-900"> Total </td>
+                    <td> {{ formatCurrency(serviceDetailTotal) }} </td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div id="footer" class="flex gap-4 my-2 justify-between text-sm/relaxed text-left">
+            <div class="flex flex-col gap-10">
+                <p>Technician</p>
+                <span>{{ $page.props.auth.user.name }}</span>
+            </div>
+            <div class="flex flex-col gap-10">
+                <p>Customer</p>
+                <span>{{ serviceDetail.service.customer.user.name }}</span>
+            </div>
+            <div class="flex flex-col items-left border px-2 mx-1">
+                <p class="font-bold">Notes!</p>
+                {{ serviceDetail.notes }}
+            </div>
         </div>
-        <div class="flex flex-col gap-10">
-            <p>Customer</p>
-            <span>{{ serviceDetail.service.customer.user.name }}</span>
+        <hr>
+        <div id="footer" class="my-2 text-sm/relaxed">
+            <p><span class="font-bold text-green-900">Cost</span> adalah biaya jasa service. <span
+                    class="font-bold text-green-900">Total</span> mencakup biaya jasa dan biaya tambahan untuk Spare
+                Part
+                yang diganti jika ada.</p>
         </div>
-        <div class="flex flex-col items-left border px-2 mx-1">
-            <p class="font-bold">Notes!</p>
-            {{ serviceDetail.notes }}
-        </div>
-    </div>
-    <hr>
-    <div id="footer" class="my-2 text-sm/relaxed">
-        <p><span class="font-bold text-green-900">Cost</span> adalah biaya jasa service. <span
-                class="font-bold text-green-900">Total</span> mencakup biaya jasa dan biaya tambahan untuk Spare Part
-            yang diganti jika ada.</p>
     </div>
 </template>
 
