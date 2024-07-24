f9893907a2756a5a3a6db2e725d6af9d78f19b43
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-07-24 22:05:02 +0800

    update rounded

--- resources/js/Components/ButtonImage.vue
+++ resources/js/Components/ButtonImage.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="inline-flex items-center p-0.5 bg-white border border-green-600 rounded font-semibold text-xs text-green-800 tracking-widest shadow hover:bg-green-50 focus:outline-none focus:ring-1 focus:ring-green-800 disabled:opacity-25 transition ease-in-out duration-150">
+        class="inline-flex items-center p-0.5 bg-white border border-green-600 rounded-md font-semibold text-xs text-green-800 tracking-widest shadow hover:bg-green-50 focus:outline-none focus:ring-1 focus:ring-green-800 disabled:opacity-25 transition ease-in-out duration-150">
         <slot />
     </button>
 </template>
