721bddd6d7f0cf3b571ee39482a3ffd58b39fb4d
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-07-31 00:15:37 +0800

    update color and add header

--- resources/js/Pages/Setting/Edit.vue
+++ resources/js/Pages/Setting/Edit.vue
@@ -86,6 +86,9 @@ const closeModal = () => {
   <Head title="Setting" />
 
   <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-green-800 leading-tight">Setting</h2>
+    </template>
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
         <div class="p-4 sm:p-8 bg-white shadow sm:rounded">
