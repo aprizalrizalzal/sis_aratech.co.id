2232971adf50273a64ef69e1c978c7caba3d1738
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-08-01 23:30:21 +0800

    update product

--- resources/js/Pages/Welcome/Product.vue
+++ resources/js/Pages/Welcome/Product.vue
@@ -109,7 +109,7 @@ const closeModal = () => {
     </div>
 
     <div class="flex flex-col items-center pt-2">
-        <SecondaryButton v-if="itemsPerPage === itemsPerPage" @click="showAllProduct" class="w-full justify-center">Show
+        <SecondaryButton v-if="itemsPerPage === 12" @click="showAllProduct" class="w-full justify-center">Show
             All
             &nbsp;
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -118,7 +118,7 @@ const closeModal = () => {
                     d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67" />
             </svg>
         </SecondaryButton>
-        <SecondaryButton v-if="itemsPerPage !== itemsPerPage" @click="hideSomeProduct" class="w-full justify-center">
+        <SecondaryButton v-if="itemsPerPage !== 12" @click="hideSomeProduct" class="w-full justify-center">
             Hiding some &nbsp;
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                 class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
